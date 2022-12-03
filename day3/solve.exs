d04324db140e4cc907a91378db978f03c43f8762
Oleg Broslavsky <yalegko@gmail.com>
2022-12-03 22:09:08 +0700

    Add day3

---
+++ day3/solve.exs
@@ -0,0 +1,34 @@
+
+[fname] = System.argv()
+File.stream!(fname)
+  |> Stream.map(&String.trim/1)
+
+  # Split in 2 halves.
+  |> Stream.map(fn line ->
+    mid = div(String.length(line), 2)
+    String.split_at(line, mid)
+  end)
+
+  # Find the matching character: half2 is interpreted as regex set and we are
+  # looking for the any matching character (as we are guaranted to have the only one).
+  |> Stream.map(fn {half1, half2} = _splitted ->
+    [matched] = Regex.compile!("[#{half2}]")
+      |> Regex.run(half1)
+    [letter] = String.to_charlist(matched)
+    letter
+  end)
+
+  # Weight the results
+  |> Stream.map(fn
+    l when l >= ?a and l <= ?z -> l - ?a + 1
+    l when l >= ?A and l <= ?Z -> l - ?A + 27
+  end)
+
+  # Sum everything.
+  |> Stream.transform(0, fn el, acc ->
+    sum =  el + acc
+    {[sum], sum}
+  end)
+  |> Stream.take(-1)
+  |> Stream.each(&IO.puts/1)
+  |> Stream.run()
---
+++ day3/solve2.exs
@@ -0,0 +1,43 @@
+
+# Returns %{a: 1, b: 1, ...} for all chars existed in the line (not counting them).
+get_unique_chars_map = fn line ->
+  line
+    |> Enum.reduce(%{}, fn char, map -> Map.put(map, char, 1) end)
+end
+
+# Sum the values of duplicated keys.
+sum_maps = fn m1, m2 ->
+  Map.merge(m1, m2, fn _key, v1, v2 -> v1 + v2 end)
+end
+
+[fname] = System.argv()
+File.stream!(fname)
+  |> Stream.map(&String.trim/1)
+  |> Stream.chunk_every(3)
+
+  # Find the symbol used in every of all 3 lines.
+  |> Stream.map(fn chunk ->
+    chunk
+      |> Enum.map(&String.graphemes/1)
+      |> Enum.map(get_unique_chars_map)
+      |> Enum.reduce(%{}, sum_maps)
+      |> Enum.max(fn {_k1, v1}, {_k2, v2} -> v1 >= v2 end)
+      |> elem(0) # Result of max is tuple {k,v}.
+      |> String.to_charlist()
+      |> Enum.fetch!(0)
+  end)
+
+  # Weight the result.
+  |> Stream.map(fn
+    l when l >= ?a and l <= ?z -> l - ?a + 1
+    l when l >= ?A and l <= ?Z -> l - ?A + 27
+  end)
+
+  # Sum everything.
+  |> Stream.transform(0, fn el, acc ->
+    sum =  el + acc
+    {[sum], sum}
+  end)
+  |> Stream.take(-1)
+  |> Stream.each(&IO.inspect/1)
+  |> Stream.run()
