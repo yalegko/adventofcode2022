4a86e77f2449c4fa1d75a54dddb5e8d04193961c
Oleg Broslavsky <yalegko@gmail.com>
2022-12-23 16:02:53 +0800

    Add day23

---
+++ day23/solve.exs
@@ -0,0 +1,133 @@
+#!/usr/bin/env elixir
+
+defmodule Field do
+  def do_step(field, round) do
+    propose(field, round)
+    |> Enum.reduce(field, fn
+      {pos, [a, b | rest]}, field -> field
+      {new_pos, [old_pos]}, field -> field |> MapSet.delete(old_pos) |> MapSet.put(new_pos)
+    end)
+  end
+
+  def propose(field, round) do
+    pattern = pattern(round)
+    |> IO.inspect(label: "Pattern")
+
+
+    field
+    |> Enum.reduce(%{}, fn elf, acc ->
+      case field |> propose_elf(elf, pattern) do
+        nil -> acc
+        pos -> Map.update(acc, pos, [elf], fn old -> [elf | old] end)
+      end
+    end)
+  end
+
+  def propose_elf(field, {x, y}, pattern) do
+    adjacent =
+      [
+        {x + 1, y}, {x, y + 1}, {x + 1, y + 1}, {x + 1, y - 1},
+        {x - 1, y}, {x, y - 1}, {x - 1, y - 1}, {x - 1, y + 1},
+      ]
+      |> Enum.reject(fn point -> MapSet.member?(field, point) end)
+
+    proposed =
+      if length(adjacent) == 8 do
+        nil
+      else
+        pattern
+        # |> IO.inspect(label: "? #{inspect({x, y})}")
+        |> Enum.map(fn
+          :N -> [{x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1}]
+          :S -> [{x - 1, y + 1}, {x, y + 1}, {x + 1, y + 1}]
+          :W -> [{x - 1, y - 1}, {x - 1, y}, {x - 1, y + 1}]
+          :E -> [{x + 1, y - 1}, {x + 1, y}, {x + 1, y + 1}]
+        end)
+        # |> IO.inspect(label: "pattern")
+        |> Enum.find(&(&1 |> Enum.all?(fn point -> not MapSet.member?(field, point) end)))
+        # |> IO.inspect(label: "Propose {#{x},#{y}}")
+      end
+
+    if proposed == nil, do: nil, else: proposed |> Enum.at(1)
+  end
+
+  def pattern(round) do
+    case rem(round, 4) do
+      0 -> [:N, :S, :W, :E]
+      1 -> [:S, :W, :E, :N]
+      2 -> [:W, :E, :N, :S]
+      3 -> [:E, :N, :S, :W]
+    end
+  end
+
+  def visualize(field) do
+    {{minx, miny}, {maxx, maxy}} = field |> corners()
+
+
+    IO.inspect({{minx, miny}, {maxx, maxy}}, label: "Frontier")
+
+    for y <- miny-1..maxy+1 do
+      for(x <- minx-1..maxx+1, do: (if MapSet.member?(field, {x, y}), do: "#", else: "."))
+      |> Enum.join()
+      |> IO.puts()
+    end
+
+    IO.puts("")
+
+    field
+  end
+
+  def corners(field) do
+    field
+    |> Enum.reduce(
+      {{100500, 100500}, {-1, -1}},
+      fn {x, y}, {{minx, miny}, {maxx, maxy}} ->
+        {
+          {min(minx, x), min(miny, y)},
+          {max(maxx, x), max(maxy, y)}
+        }
+      end
+    )
+  end
+end
+
+
+[fname] = System.argv()
+
+field =
+  fname
+  |> File.stream!()
+  |> Stream.map(&String.trim/1)
+  |> Stream.map(&String.graphemes/1)
+  |> Stream.map(&Enum.with_index/1)
+  |> Stream.with_index(0)
+  |> Stream.flat_map(fn {line, y} ->
+    line
+    |> Enum.filter(fn {c, _x} -> c == "#" end)
+    |> Enum.map(fn {_c, x} -> {x, y} end)
+  end)
+  |> Stream.scan(MapSet.new(), fn {x, y}, acc -> MapSet.put(acc, {x, y}) end)
+  |> Stream.take(-1)
+  |> Enum.at(0)
+  |> Field.visualize()
+
+
+moved =
+  for i <- 0..9, reduce: field do
+    field ->
+      # IO.puts("Round #{i}")
+      field
+      |> Field.do_step(i)
+      # |> Field.visualize()
+  end
+
+{{minx, miny}, {maxx, maxy}} =
+  moved
+  |> Field.corners()
+  |> IO.inspect(label: "Corners:")
+
+MapSet.size(field)
+|> IO.inspect()
+
+(maxx - minx + 1)*(maxy - miny + 1) - MapSet.size(moved)
+|> IO.inspect(label: "Free space")
---
+++ day23/solve2.exs
@@ -0,0 +1,124 @@
+#!/usr/bin/env elixir
+
+defmodule Field do
+  def do_step(field, round) do
+    propose(field, round)
+    |> Enum.reduce(field, fn
+      {pos, [a, b | rest]}, field -> field
+      {new_pos, [old_pos]}, field -> field |> MapSet.delete(old_pos) |> MapSet.put(new_pos)
+    end)
+  end
+
+  # Returns a map of new positions to the list of elves who proposed it.
+  defp propose(field, round) do
+    pattern = pattern(round)
+
+    field
+    |> Enum.reduce(%{}, fn elf, acc ->
+      case field |> propose_elf(elf, pattern) do
+        nil -> acc
+        pos -> Map.update(acc, pos, [elf], fn old -> [elf | old] end)
+      end
+    end)
+  end
+
+  # Returns `nil` or the movement ({new_x, new_y}) proposed by the elf.
+  defp propose_elf(field, {x, y} = elf, pattern) do
+    taken_adjacent =
+      pattern
+      |> Enum.flat_map(&(adjacent_to(elf, &1)))
+      |> MapSet.new()
+      |> MapSet.intersection(field)
+      |> MapSet.size()
+
+    proposed =
+      if taken_adjacent == 0 do
+        nil
+      else
+        pattern
+        |> Enum.map(&(adjacent_to(elf, &1)))
+        |> Enum.find(&(&1 |> Enum.all?(fn point -> not MapSet.member?(field, point) end)))
+      end
+
+    # All moves are ordered as [diagonal, move, diagonal], so we need a middle one.
+    if proposed == nil, do: nil, else: proposed |> Enum.at(1)
+  end
+
+  defp adjacent_to({x, y}, :N), do: [{x - 1, y - 1}, {x, y - 1}, {x + 1, y - 1}]
+  defp adjacent_to({x, y}, :S), do: [{x - 1, y + 1}, {x, y + 1}, {x + 1, y + 1}]
+  defp adjacent_to({x, y}, :W), do: [{x - 1, y - 1}, {x - 1, y}, {x - 1, y + 1}]
+  defp adjacent_to({x, y}, :E), do: [{x + 1, y - 1}, {x + 1, y}, {x + 1, y + 1}]
+
+  defp pattern(round) do
+    case rem(round, 4) do
+      0 -> [:N, :S, :W, :E]
+      1 -> [:S, :W, :E, :N]
+      2 -> [:W, :E, :N, :S]
+      3 -> [:E, :N, :S, :W]
+    end
+  end
+
+  def visualize(field) do
+    {{minx, miny}, {maxx, maxy}} = field |> corners()
+
+
+    IO.inspect({{minx, miny}, {maxx, maxy}}, label: "Size of Field")
+
+    for y <- miny-1..maxy+1 do
+      for(x <- minx-1..maxx+1, do: (if MapSet.member?(field, {x, y}), do: "#", else: "."))
+      |> Enum.join()
+      |> IO.puts()
+    end
+
+    IO.puts("")
+
+    field
+  end
+
+  # Returns {left,top} {right, bottom} corners of the Field.
+  def corners(field) do
+    field
+    |> Enum.reduce(
+      {{100500, 100500}, {-1, -1}},
+      fn {x, y}, {{minx, miny}, {maxx, maxy}} ->
+        {
+          {min(minx, x), min(miny, y)},
+          {max(maxx, x), max(maxy, y)}
+        }
+      end
+    )
+  end
+end
+
+
+[fname] = System.argv()
+
+field =
+  fname
+  |> File.stream!()
+  |> Stream.map(&String.trim/1)
+  |> Stream.map(&String.graphemes/1)
+  |> Stream.map(&Enum.with_index/1)
+  |> Stream.with_index(0)
+  |> Stream.flat_map(fn {line, y} ->
+    line
+    |> Enum.filter(fn {c, _x} -> c == "#" end)
+    |> Enum.map(fn {_c, x} -> {x, y} end)
+  end)
+  |> Stream.scan(MapSet.new(), fn {x, y}, acc -> MapSet.put(acc, {x, y}) end)
+  |> Stream.take(-1)
+  |> Enum.at(0)
+  |> Field.visualize()
+
+
+0..9999
+|> Stream.transform(field, fn i, field ->
+  stepped = field |> Field.do_step(i)
+
+  if MapSet.equal?(stepped, field),
+    do: {:halt, field},
+    else: {[i+2], stepped}
+end)
+|> Stream.take(-1)
+|> Enum.at(0)
+|> IO.inspect(label: "Final step")
